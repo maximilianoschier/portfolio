32958bdba8a75409f5d9677e9e83ffd478811fe3
Maxi Schier <maximiliano.schier@gmail.com>
2024-06-16 00:13:10 +0200

    Remove / from api_url

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    api_url="https://portfolio-mschier.vercel.app:8000/"
+    api_url="https://portfolio-mschier.vercel.app:8000"
 )
