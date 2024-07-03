a3b0d3deeaf1452cdfb563150a10a5ccb13626f8
Maxi Schier <maximiliano.schier@gmail.com>
2024-07-03 21:30:19 +0200

    Move data dir content to root dir

--- portfolio/data.py
+++ portfolio/data.py
@@ -69,7 +69,7 @@ class Data:
             self.training = [Info(**info) for info in training]
             # self.extras = [Extra(**info) for info in extras]
 
-with open("assets/data/data.json") as file:
+with open("assets/data.json") as file:
     json_data = json.load(file)
 
 data = Data(**json_data)
